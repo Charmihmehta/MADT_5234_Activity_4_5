2a5a5521a55c5d06164fc757d50bacc5f8122cd5
Charmi Mehta <charmimehta@Charmis-MacBook-Pro.local>
2019-01-16 14:35:01 -0500

    Third commit Green  factor

--- src/TribalScreaming.java
+++ src/TribalScreaming.java
@@ -3,16 +3,19 @@ public class TribalScreaming {
 	
 	public String scream(String name)
 	{
-		if(name != null && name != name.toUpperCase() )
-		{
-			return name + " is amazing";
+		if(name == null) {
+			return "You are amazing";
 		}
+		else {
+			if(name == name.toUpperCase()) {
+				return name + " IS AMAZING";
+			}
+			else {
+				return name + " is amazing";
+			}
+		}
+
 		
-		else if(name == "" || name == null)
-		{
-		return "You are amazing";
-		}
-		return name;
 	}
 
 }
