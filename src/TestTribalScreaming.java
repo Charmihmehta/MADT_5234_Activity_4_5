238154b34ca53da756dc34eff6bd0b499fec5ec1
Charmi Mehta <charmimehta@Charmis-MacBook-Pro.local>
2019-01-16 15:41:11 -0500

    Fifth commit Red  factor

--- src/TestTribalScreaming.java
+++ src/TestTribalScreaming.java
@@ -7,9 +7,15 @@ class TestTribalScreaming {
  * 1) One person is amazing -> Peter is amazing
  * 2) Null -> You are amazing
  * 3)Uppercase -> PETER IS AMAZING
+ * 4) accept 2 names -> Peter and Jigesha are amazing
+ * 5) More than 2 person -> Peter, Jigesha, and Marcos are amazing
+ * 6) Jumble more names -> Peter, Marcos, and Albert are amazing. JIGESHA ALSO!
+
  * 
  * stub scream(name)
  */
+	
+	// First factor
 	@Test
 	public void testOnePerson() {
 		TribalScreaming t = new TribalScreaming();
@@ -17,6 +23,7 @@ class TestTribalScreaming {
 		assertEquals("Peter is amazing",result);
 		
 	}
+	// Second Factor
 	@Test
 	public void testNullPerson() {
 		TribalScreaming t = new TribalScreaming();
@@ -24,6 +31,8 @@ class TestTribalScreaming {
 		assertEquals("You are amazing",result);
 		
 	}
+	
+	// Third Factor
 	@Test
 	public void testPersonShouting() {
 		TribalScreaming t = new TribalScreaming();
@@ -31,13 +40,22 @@ class TestTribalScreaming {
 		assertEquals("PETER IS AMAZING",result);
 		
 	}
-	
+	// Fourth factor
 	@Test
 	public void testTwoPersonShouting() {
 		TribalScreaming t = new TribalScreaming();
 		String[] expected_op = new String[] {"Peter","Jigesha"};
 		String result = t.scream_name(expected_op);
 		assertEquals("Peter and Jigesha are amazing",result);
+		
+	}
+	// Fifth factor
+	@Test
+	public void testTwoOrMorePersonShouting() {
+		TribalScreaming t = new TribalScreaming();
+		String[] expected_op = new String[] {"Peter","Jigesha","Marcos"};
+		String result = t.scream_name(expected_op);
+		assertEquals("Peter, Jigesha and Marcos are amazing",result);
 		
 	}
 	
